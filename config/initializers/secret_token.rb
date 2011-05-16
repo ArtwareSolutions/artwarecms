0cef31595985e088de34004ea67b299a9bd9927b
Simon LETORT <simon@artwaresolutions.org>
2011-05-16 09:33:44 -0400

    launch it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtwareCMS::Application.config.secret_token = '4d9039fdfe8dddde4f8e309d4ea2e20b0e426d641ce07976a1041f1beabbc564c6de3ab8084c0ddf08156b637477215daa635d0bff56b622238edf0749258fbe'
